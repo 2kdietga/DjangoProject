45f7439bbab5eb80894e5b66771e93de070ad141
wind <wind@gmail.com>
2025-09-29 17:20:45 +0700

    thêm các liên kết cần thiết và hiển thị khác khi cart empty

--- carts/views.py
+++ carts/views.py
@@ -3,7 +3,7 @@ from store.models import Product
 from .models import Cart, CartItem
 from django.http import HttpResponse
 
-def __cart_id(request):
+def _cart_id(request):
     cart = request.session.session_key
     if not cart:
         cart = request.session.create()
@@ -12,9 +12,9 @@ def __cart_id(request):
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     try:
-        cart = Cart.objects.get(cart_id=__cart_id(request))
+        cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
-        cart = Cart.objects.create(cart_id=__cart_id(request))
+        cart = Cart.objects.create(cart_id=_cart_id(request))
     cart.save()
     
     try:
@@ -32,7 +32,7 @@ def add_cart(request, product_id):
 
 
 def remove_cart(request, product_id):
-    cart = Cart.objects.get(cart_id=__cart_id(request))
+    cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product,id=product_id)
     cart_item = CartItem.objects.get(product=product, cart=cart)
     if cart_item.quantity > 1:
@@ -44,7 +44,7 @@ def remove_cart(request, product_id):
 
 
 def remove_cart_item(request, product_id):
-    cart = Cart.objects.get(cart_id=__cart_id(request))
+    cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product,id=product_id)
     cart_item = CartItem.objects.get(product=product, cart=cart)
     cart_item.delete()
@@ -53,7 +53,7 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
-        cart = Cart.objects.get(cart_id=__cart_id(request))
+        cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
