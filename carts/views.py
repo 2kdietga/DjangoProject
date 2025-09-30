6e41c20f3b89cfa3b71f660cd07fe9592a5d0512
wind <wind@gmail.com>
2025-09-30 17:28:03 +0700

    post và nhận rồi lọc xem variation có phải trong db, thêm vào Product thuộc tính variation

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product , Variation
 from .models import Cart, CartItem
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
@@ -11,11 +11,18 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    size = request.GET['size']
-    return HttpResponse(f"{color} {size}")
-
     product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            try:# xác nhận variation có tồn tại trong db ko
+                variation = Variation.objects.get(product=product,variation_category__iexact=key, variation_value__iexact=value) #.iexact = không phân biệt hoa thường
+                product_variation.append(variation) #product_variation = [color, size]  
+            except:
+                pass
+    
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
