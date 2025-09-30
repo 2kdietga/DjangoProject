0dc7df487c83510827b3951d9d07cdc7eb3edd8a
wind <wind@gmail.com>
2025-09-30 11:19:03 +0700

    làm Paginator (STT Page)

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from .models import Category
 from .models import Product
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def store(request, category_slug=None):
     categories= None
@@ -11,11 +12,14 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories=get_object_or_404(Category, slug=category_slug)
         products=Product.objects.filter(category=categories, is_available=True)
-        product_count=products.count()
     else:
         products=Product.objects.all().filter(is_available=True)
-        product_count=products.count()
-    context={'products':products,
+        
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+    product_count=products.count()
+    context={'products':paged_products,
              'product_count':product_count}
     return render(request, 'store/store.html',context)
 
