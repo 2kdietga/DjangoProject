dec1d8fedc78dba01573fa04c754cbcd2d2d7e21
wind <wind@gmail.com>
2025-10-03 22:11:49 +0700

    hoàn thiện phần register ( chưa thêm css cho thông báo invalid)

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -138,3 +138,9 @@ STATICFILES_DIRS = [BASE_DIR / 'DjangoProject/static']
 # Media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
